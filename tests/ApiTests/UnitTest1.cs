3f4d98126743842fe65a6d6c7b04c71679bd4a2b
marcos <marcos.agni@gmail.com>
2024-09-23 18:35:58 +0200

    Agregar archivos de proyecto.

---
+++ tests/ApiTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ApiTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
