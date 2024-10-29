61c4a9ded46a32456e37803b71f392388667894e
marcos <marcos.agni@gmail.com>
2024-10-29 15:18:44 +0100

    Incident history delete endpoint eliminated as is not posible to eliminate it

--- src/Domain/Interfaces/IIncidentHistoryRepository.cs
+++ src/Domain/Interfaces/IIncidentHistoryRepository.cs
@@ -1,7 +1,52 @@
-﻿using Domain.Entities;
+﻿using Domain.Dtos.CommonDtos.Request;
+using Domain.Dtos.CommonDtos.Response;
+using Domain.Entities;
+using Domain.Entities.Common;
 
 namespace Domain.Interfaces;
 
-public interface IIncidentHistoryRepository : IGenericRepository<IncidentHistory>
+public interface IIncidentHistoryRepository
 {
+    /// <summary>
+    /// Asynchronously adds a new entity to the database.
+    /// </summary>
+    /// <param name="entity">The entity to be added.</param>
+    /// <returns>A task representing the asynchronous add operation.</returns>
+    public Task AddAsync(IncidentHistory entity);
+
+    /// <summary>
+    /// Asynchronously retrieves a paginated list of entities from the database, applying optional query filters for sorting and searching.
+    /// </summary>
+    /// <param name="queryFilter">Query filter data for sorting, searching, and pagination.</param>
+    /// <returns>A task representing the asynchronous operation, returning a paginated list of entities.</returns>
+    public Task<PaginatedList<IncidentHistory>> GetAsync(QueryFilterDto queryFilter);
+
+    /// <summary>
+    /// Asynchronously retrieves a specific entity by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the entity to retrieve.</param>
+    /// <returns>A task representing the asynchronous operation, returning the entity if found, or null if not.</returns>
+    public Task<IncidentHistory?> GetByIdAsync(long id);
+
+    /// <summary>
+    /// Asynchronously updates an existing entity in the database.
+    /// </summary>
+    /// <param name="entity">The updated data of the entity.</param>
+    /// <returns>A task representing the asynchronous update operation.</returns>
+    public Task UpdateAsync(IncidentHistory entity);
+
+    /// <summary>
+    /// Asynchronously counts the number of entities in the database based on optional filtering criteria.
+    /// </summary>
+    /// <param name="queryFilter">Query filter data for sorting, searching, and pagination.</param>
+    /// <param name="filterParameter">Additional parameters to filter the count operation.</param>
+    /// <returns>A task representing the asynchronous count operation, returning the total count of entities.</returns>
+    public Task<int> CountAsync(QueryFilterDto queryFilter, List<string>? filterParameter);
+
+    /// <summary>
+    /// Asynchronously checks whether an entity with the specified identifier exists in the database.
+    /// </summary>
+    /// <param name="id">The unique identifier of the entity to check.</param>
+    /// <returns>A task representing the asynchronous operation, returning true if the entity exists, otherwise false.</returns>
+    public Task<bool> ExistsAsync(long id);
 }
--- src/Infraestructure/Repositories/IncidentHistoryRepository.cs
+++ src/Infraestructure/Repositories/IncidentHistoryRepository.cs
@@ -1,11 +1,85 @@
 ﻿using AutoMapper;
+using Domain.Dtos.CommonDtos.Request;
+using Domain.Dtos.CommonDtos.Response;
 using Domain.Entities;
+using Domain.Entities.Common;
 using Domain.Interfaces;
+using Infraestructure.Helpers;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infraestructure.Repositories;
 
-public class IncidentHistoryRepository(AppDbContext context, IMapper mapper) : GenericRepository<IncidentHistory>(context, mapper), IIncidentHistoryRepository
+public class IncidentHistoryRepository : IIncidentHistoryRepository
 {
+    private readonly AppDbContext _dbContext;
+    private readonly DbSet<IncidentHistory> _dbSet;
+    private readonly IMapper _mapper;
 
+    public IncidentHistoryRepository(AppDbContext context, IMapper mapper) 
+    {
+        _dbContext = context;
+        _mapper = mapper;
+        _dbSet = _dbContext.Set<IncidentHistory>();
+    }
+
+    /// <inheritdoc/>
+    public virtual async Task AddAsync(IncidentHistory entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    /// <inheritdoc/>
+    public async Task<PaginatedList<IncidentHistory>> GetAsync(QueryFilterDto queryFilter)
+    {
+        List<string> searchParameters = new List<string>();
+
+        var totalCount = await CountAsync(queryFilter, searchParameters);
+
+        IQueryable<IncidentHistory> query = _dbSet.AsQueryable();
+
+        query = new QueryFilterBuilder<IncidentHistory>(_dbSet)
+            .ApplyQueryFilter(queryFilter, searchParameters)
+            .Build();
+
+        var items = await query.ToListAsync();
+
+        return new PaginatedList<IncidentHistory>(items, totalCount);
+    }
+
+    /// <inheritdoc/>
+    public async Task<IncidentHistory?> GetByIdAsync(long id)
+    {
+        return await _dbSet
+            .Where(x => x.Id == id)
+            .FirstOrDefaultAsync();
+    }
+
+    /// <inheritdoc/>
+    public async Task UpdateAsync(IncidentHistory entity)
+    {
+        _dbSet.Update(entity);
+    }
+
+    /// <inheritdoc/>
+    public async Task<bool> ExistsAsync(long id)
+    {
+        return await _dbSet
+            .Where(x => x.Id == id)
+            .AnyAsync();
+    }
+
+    /// <inheritdoc/>
+    public virtual async Task<int> CountAsync(QueryFilterDto queryFilter, List<string>? searchParameters)
+    {
+        IQueryable<IncidentHistory> query = _dbSet.AsQueryable();
+
+        if (!string.IsNullOrEmpty(queryFilter.Search))
+        {
+            query = new QueryFilterBuilder<IncidentHistory>(_dbSet)
+            .Where(searchParameters, queryFilter.Search)
+            .Build();
+        }
+
+        return await query.CountAsync();
+    }
 }
