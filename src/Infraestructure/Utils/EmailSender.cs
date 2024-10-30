465a5d6c71d5b7910f55396a6aa3a9b655d08a15
marcos <marcos.agni@gmail.com>
2024-10-30 23:26:34 +0100

    Email sender to send email when password forgoten

---
+++ src/Infraestructure/Utils/EmailSender.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Interfaces.Utils;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Utils;
+
+public class EmailSender : IEmailSender
+{
+    public async Task SendRecoverPasswordAsync(string email, string token)
+    {
+        var mail = Environment.GetEnvironmentVariable("EMAIL");
+        var password = Environment.GetEnvironmentVariable("EMAIL_PASSWORD");
+
+        var smtpClient = new SmtpClient("smtp.gmail.com", 587)
+        {
+            Credentials = new NetworkCredential(mail, password),
+            EnableSsl = true,
+            UseDefaultCredentials = false,
+        };
+
+        var url = $"https://miguelPasaLaUrl/reset-password?token={token}";
+        try {
+            await smtpClient.SendMailAsync(
+                new MailMessage(
+                    from: mail,
+                    to: email,
+                    subject: "Recuperación de Contraseña",
+                    body: $"Hola,\n\nPara recuperar tu contraseña, por favor haz clic en el siguiente enlace:\n\n{url}\n\nSi no solicitaste la recuperación de la contraseña, ignora este mensaje."));
+
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+}
