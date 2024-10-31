1804e66495faa83ce4d6841d5f9fe856002d1166
marcos <marcos.agni@gmail.com>
2024-10-31 19:36:33 +0100

    Changes in the docker compose, disable https until cert is available, also move the docker env variables to env file, (dockerignored)

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -32,5 +32,6 @@ public class Program
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
+                webBuilder.UseUrls("http://0.0.0.0:5000");
             });
 }
